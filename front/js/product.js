9adfb910eff930cd2d9cb47d0a2c7fb28f554f36
mikafrdev <mikadevfr@gmail.com>
2022-02-22 12:23:39 +0100

    Clean JS page Product

--- front/js/product.js
+++ front/js/product.js
@@ -67,9 +67,6 @@ class Order {
         let colorSelected = document.getElementById("colors").value
         let quantitySelected = document.getElementById("quantity").value
 
-        //console.log("colorSelected : "+colorSelected)
-        //console.log("quantitySelected : "+quantitySelected)
-
         if (colorSelected == "" || quantitySelected == 0) {
             return false
         }else{
@@ -79,17 +76,10 @@ class Order {
 
     isColorStillOrdered = (orderValueLS, colorChoice) => {
 
-        //console.log("isColorStillOrdered")
-        //console.log(orderValueLS, colorChoice)
-
         let i = 0
         while (i < orderValueLS.length) {
 
-            console.log("boucle : ", orderValueLS[i].color)
-
-            if ( colorChoice == orderValueLS[i].color ) {
-                return i
-            }
+            if ( colorChoice == orderValueLS[i].color ) return i
             i++
         }
         return -1
@@ -97,89 +87,39 @@ class Order {
 
     //Au clic sur le bouton "Ajouter au panier"
     addOrder = (product) => {
-
-        console.log("addOrder")
  
         if (this.isValideForm()) {
-
-            console.log("Formulaire valide")
-
-            //const getKeyFromLocalStorage = JSON.parse(localStorage.key(product._id))
-            const getKeyFromLocalStorage = localStorage.key(product._id)
+            const getValueFromLocalStorage = localStorage.getItem(product._id)
+            const valueFronLocalStorage = JSON.parse(getValueFromLocalStorage)
             const colorChoice = document.getElementById('colors').value
             const quantityChoice = document.getElementById('quantity').value
-
-            console.log("Récup KEY", localStorage.getItem(localStorage.key(product._id)))
-
             let NewOrderValue = {}
 
-            if(getKeyFromLocalStorage == product._id){
+            if(valueFronLocalStorage){
 
-                const getOrderValueLS = JSON.parse(localStorage.getItem(localStorage.key(product._id)))
-                console.log("OUI : ", getOrderValueLS)
-                
-                //console.log("orderValueLS.color et colorChoice : ", orderValueLS.colors, colorChoice)
-
-                //console.log("orderValueLS en test : ", orderValueLS.colors.includes(colorChoice))
-                //console.log("orderValueLS.colors[0] en test : ", orderValueLS.colors[0])
-
-                //console.log("TEST COLOR", this.isColorStillOrdered (orderValueLS, colorChoice))
-                
-                //console.log("TEST TABLEAU : ", orderValueLS.length)
-                //console.log("TEST TABLEAU : ", orderValueLS.colors['color'])
-
-                //const indexColorInLS = orderValueLS.colors[0].indexOf(colorChoice)
-                //console.log("TEST indexColorInLS : ", indexColorInLS)
-
-                const indexSameColor = this.isColorStillOrdered (getOrderValueLS, colorChoice)
-
-                console.log("indexSameColor : ", indexSameColor)
+                const indexSameColor = this.isColorStillOrdered (valueFronLocalStorage, colorChoice)
 
                 if(indexSameColor != -1){
 
-                    console.log("Couleur présente")
-
-                    NewOrderValue = getOrderValueLS
-
+                    NewOrderValue = valueFronLocalStorage
                     NewOrderValue[indexSameColor].quantity = quantityChoice
-
-                    
-
                 }else{
 
-                    console.log("Couleur non présente")
-
                     NewOrderValue = {color: colorChoice, quantity: quantityChoice}
-
-                    console.log("NewOrderValue", NewOrderValue)
-
-                    getOrderValueLS.push(NewOrderValue)
-
-                    NewOrderValue = getOrderValueLS
-
-                    console.log("NewOrderValue", NewOrderValue)
-
+                    valueFronLocalStorage.push(NewOrderValue)
+                    NewOrderValue = valueFronLocalStorage
                 }
 
             }else{
-
-                //{"order":["Blue", 1] ["green, 2"]}
-                //{"order":["Blue", 1], "order":["green, 2"]}
 
                 NewOrderValue = [
                     {color: colorChoice, quantity: quantityChoice}
                 ]
             }
 
-            console.log(NewOrderValue)
-
-
-
-            //let OrderKeyStringified = JSON.stringify(product._id)
             let OrderValueStringified = JSON.stringify(NewOrderValue)
-
-            //localStorage.setItem(OrderKeyStringified, OrderValueStringified)
             localStorage.setItem(product._id, OrderValueStringified)
+
         }else{
     
             alert("Formulaire non valide")
