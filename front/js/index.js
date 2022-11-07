871559d94ea96cb58cb8510cf35ac97a09491d48
Mikafrdev <mikadevfr@gmail.com>
2022-11-07 17:53:49 +0100

    HP - maj displayProducts | ajout des commentaires

---
+++ front/js/index.js
@@ -0,0 +1,30 @@
+//Affiche les canapés passés en paramètre en produisant du code HTML
+const displayProducts = (productsData) => {
+
+    console.log("displayKanaps")
+    let htmlContent
+
+    for(const val in productsData){
+        htmlContent = `
+        <a href="./product.html?id=${productsData[val]._id}">
+            <article>
+                <img src="${productsData[val].imageUrl}" alt="${productsData[val].altTxt}" />
+                <h3 class="productName">${productsData[val].name}</h3>
+                <p class="productDescription">${productsData[val].description}</p>
+            </article>
+        </a>
+            `
+        document.querySelector('#items').insertAdjacentHTML('afterbegin', htmlContent)
+    }
+}
+
+const main = async () => {
+    
+    //Récupération des données de tous les produitss avec un argument vide passé en paramètre de la fonction <getMockedData>
+    const productsData = await getMockedData("")
+    
+    //Affichage de chacun des produits
+    displayProducts(productsData)
+}
+
+main()
